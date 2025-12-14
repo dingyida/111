7ddd40de3b156d729224ac5f918c767d42252d97
dingyida <95954864+dingyida@users.noreply.github.com>
2025-12-14 16:34:01 -0500

    Delete object directory

--- object/list.py
+++
@@ -1,7 +0,0 @@
-import carla
-client = carla.Client('localhost', 2000)
-client.set_timeout(5.0)
-world = client.get_world()
-for bp in world.get_blueprint_library().filter('*trash*can*'):
-    print(bp.id)
-
--- object/town_name.py
+++
@@ -1,3 +0,0 @@
-import carla
-client = carla.Client('localhost', 2000)
-print(client.get_world().get_map().name)
